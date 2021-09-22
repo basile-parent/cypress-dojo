c87ead919ac1689245914153f186c2e56212c953
Basile Parent <basile.parent@ext.leroymerlin.fr>
2021-09-22 15:55:13 +0200

    Complete tests

--- cypress/integration/cucumber.feature
+++ cypress/integration/cucumber.feature
@@ -1,4 +1,22 @@
 Feature: Add a todo item
-  Scenario: Should displays two todo items by default
+  Scenario: List have two todo items by default
     Given I'm on the todo list
     Then I should see 2 items
+    And item number 1 should be "Pay electric bill"
+    And item number 2 should be "Walk the dog"
+
+  Scenario: Add new item on submit
+    Given I'm on the todo list
+    When I type "New Item" on the input and validate
+    Then I should see 3 items
+
+  Scenario: Strike item when it's complete
+    Given I'm on the todo list
+    When I check the item "Pay electric bill"
+    Then the item "Pay electric bill" be striked
+
+  Scenario: Edit todo on double click
+    Given I'm on the todo list
+    When I double click on item "Walk the dog"
+    And I change the label to "New item text" and validate
+    Then the item label has changed to "New item text"
--- cypress/integration/cucumber/cucumber.step.js
+++ cypress/integration/cucumber/cucumber.step.js
@@ -4,6 +4,42 @@ Given("I'm on the todo list", () => {
     cy.visit("https://example.cypress.io/todo");
 });
 
-Then("I should see 2 items", () => {
-    cy.get(".todo-list li").should("have.length", 2);
+Then("I should see {int} items", (itemCount) => {
+    cy.get(".todo-list li").should("have.length", itemCount);
+});
+
+Then("item number {int} should be {string}", (itemIndex, label) => {
+    cy.get(`.todo-list li:nth-of-type(${ itemIndex })`).should("have.text", label);
+});
+
+When("I type {string} on the input and validate", (newItemLabel) => {
+    cy.get('input.new-todo').type(`${newItemLabel}{enter}`);
+});
+
+When("I check the item {string}", (itemLabel) => {
+    cy.contains(itemLabel)
+        .parent()
+        .find("input[type=checkbox]")
+        .check();
+});
+
+Then("the item {string} be striked", (itemLabel) => {
+    cy.contains(itemLabel)
+        .parents("li")
+        .should("have.class", "completed");
+});
+
+When("I double click on item {string}", (itemLabel) => {
+    cy.contains(itemLabel)
+        .dblclick();
+});
+
+When("I change the label to {string} and validate", (itemLabel) => {
+    cy.get("input.edit")
+        .clear()
+        .type(`${itemLabel}{enter}`);
+});
+
+Then("the item label has changed to {string}", (itemLabel) => {
+    cy.contains(itemLabel);
 });
